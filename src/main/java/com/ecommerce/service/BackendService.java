0a84d03f99e8d17783fa442e9490be60f4804871
andyweij <andyweij@yahoo.com.tw>
2023-04-25 19:54:12 +0800

    update findallgoods

--- src/main/java/com/ecommerce/service/BackendService.java
+++ src/main/java/com/ecommerce/service/BackendService.java
@@ -1,11 +1,15 @@
 package com.ecommerce.service;
 
-import java.util.Optional;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.ecommerce.dao.BackEndDao;
+import com.ecommerce.dao.BackEndJPADao;
+import com.ecommerce.entity.BeverageGoods;
 import com.ecommerce.vo.GenericPageable;
 import com.ecommerce.vo.GoodsDataCondition;
 import com.ecommerce.vo.GoodsDataInfo;
@@ -15,9 +19,11 @@ import com.ecommerce.vo.GoodsDataInfo;
 public class BackendService {
 
 	private static Logger logger = LoggerFactory.getLogger(BackendService.class);
+	
 	@Autowired
 	private BackEndDao backEndDao;
-	
+	@Autowired
+	private BackEndJPADao backEndJpaDao;
 	public GoodsDataInfo queryGoodsData(GoodsDataCondition condition,GenericPageable genericPageable) {
 		
 		GoodsDataInfo goodsDataInfo=backEndDao.queryGoodsDatabyKey(condition, genericPageable);
@@ -25,5 +31,11 @@ public class BackendService {
 		
 		return goodsDataInfo;
 	}
-	
+public List<BeverageGoods> queryAllGoods() {
+		
+		List<BeverageGoods> beverageGoods=backEndJpaDao.findAll();
+		
+		
+		return beverageGoods;
+	}
 }
